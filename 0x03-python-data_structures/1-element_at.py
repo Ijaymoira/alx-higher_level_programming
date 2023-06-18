fb1a4db386dc37e936be75951ccda08158b75de5
Ijaymoira <Ijeomamokogwu@gmail.com>
2023-06-18 16:51:47 +0100

    selecting values from a list

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -4,9 +4,9 @@ def element_at(my_list, idx):
         length = len(my_list)
         if idx == 0:
             return my_list[idx]
-        elif idx < length:
+        elif idx < len(my_list):
             return None
-        elif idx >= length :
+        elif idx == 1 and len(my_list) == 1:
             return None
-        elif idx in my_list:
+        else:
             return my_list[idx]
