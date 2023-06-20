f67d1a30786cd2e51ec851d6fb42717f5f6b0dc1
Ijaymoira <Ijeomamokogwu@gmail.com>
2023-06-20 23:08:47 +0100

    Printing digits seperated by a comma

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,5 +2,5 @@
 for x in range(0, 100):
     if x < 99:
         print("{:02d}".format(x), end=", ")
-    else:
-        print("{:d} ".format(x), end="")
+    if x > 98:
+        print("{:d} ".format(x))
