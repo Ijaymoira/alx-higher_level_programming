ee04866862535a38a5ed7ffcaca2a525def2d6f1
Ijaymoira <Ijeomamokogwu@gmail.com>
2023-06-21 00:10:46 +0100

    Adding values

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(10, 10):
+    for j in range(1):
+        if i != j:
+            print("{:02d}".format(i), end=", ")
+        #if i == :
+                #print("{:02d}".format(i))
