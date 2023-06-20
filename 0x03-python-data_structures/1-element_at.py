b22e304011cce418bdb6bc6fb7632471b67adb7c
Ijaymoira <Ijeomamokogwu@gmail.com>
2023-06-20 19:07:56 +0100

    Returning the value of index

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for x in my_list:
-        length = len(my_list)
-        if idx == 0:
+        if idx == my_list.index(x):
             return my_list[idx]
-        elif idx < len(my_list):
+        if idx < 0:
             return None
-        elif idx == 1 and len(my_list) > 1:
+        if idx == 0 and len(my_list) == 1:
             return my_list[idx]
-        elif idx == 1 and len(my_list) == 1:
+        if idx == 1 and len(my_list) == 1:
             return None
-        else:
-            return my_list[idx]
+        if idx > len(my_list):
+            return None
